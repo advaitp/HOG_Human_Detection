0b9554ecb44a6185cda2e4128bf0c23e263fa37b
Gaurav <gauraut@umd.edu>
2021-10-17 18:24:25 -0400

    Added the stub classes for Human and Box. Edited the CMakeLists to include the headers for these clases and switched the coverage to 'ON'

---
+++ include/Box.h
@@ -0,0 +1,27 @@
+///============================================================================
+/// @file        : Box.h
+/// @author      : gauraut (Driver)
+/// @author      : Advait Patole (Navigator)
+/// @version     : 1.0
+/// @copyright   : MIT License
+/// @brief       : Box.h include file
+///============================================================================
+
+#ifndef INCLUDE_BOX_H_
+#define INCLUDE_BOX_H_
+
+#include <opencv2/opencv.hpp>
+
+/// @class Box
+/// @brief This is a Box class which will be used to draw
+/// boxes around the detected human
+///
+class Box {
+ public:
+  int coordinates[4];  // The four co-ordinates will show the upper
+  // left and lower right  and y coordinates [x1, y1, x2, y2]
+
+  cv::Rect box;  // Will inherit attributes from cv::Rect
+};
+
+#endif  // INCLUDE_BOX_H_
