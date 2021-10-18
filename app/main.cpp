8863f6394b2abdac1d48deef1895736a3f4dd33f
Gaurav <gauraut@umd.edu>
2021-10-18 03:00:36 -0400

    Started with the main file for demo of our API

---
+++ app/main.cpp
@@ -0,0 +1,20 @@
+///============================================================================
+/// @file        : main.cpp
+/// @author      : gauraut (Driver)
+/// @author      : Advait Patole (Navigator)
+/// @version     : 1.0.1
+/// @copyright   : MIT License
+/// @brief       : msin.cpp Main function to run our demo
+///============================================================================
+
+/// @fn int main()
+/// @brief This is our main function to run a demo for our API
+///
+/// @pre
+/// @post
+/// @return
+int main() {
+  Camera webcam(0);
+  Detection human_detection_demo();
+  return 0;
+}
